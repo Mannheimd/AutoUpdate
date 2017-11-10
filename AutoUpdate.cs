2a66674a1896fcb10b478c838cb8891036449bf0
Chris Manders <cmanders159@gmail.com>
2017-11-10 17:56:23 +0000

    Moved Assets to TempCodeStorage

--- AutoUpdate.cs
+++ AutoUpdate.cs
@@ -235,62 +235,6 @@ namespace AutoUpdate
         }
     }
 
-    class Asset
-    {
-        public AssetType type { get; }
-        public Version version { get; }
-
-        public Asset(AssetType type, Version version)
-        {
-            this.type = type;
-            this.version = version;
-        }
-    }
-
-    class RegistrySubKeyAsset : Asset
-    {
-        public string subKeyPath { get; }
-
-        public RegistrySubKeyAsset(string subKeyPath, Version version) : base(AssetType.RegistrySubKey, version)
-        {
-            this.subKeyPath = subKeyPath;
-        }
-    }
-
-    class RegistryValueAsset : Asset
-    {
-        public string subKeyPath { get; }
-        public string value { get; }
-
-        public RegistryValueAsset(string subKeyPath, string value, Version version) : base(AssetType.RegistryValue, version)
-        {
-            this.subKeyPath = subKeyPath;
-            this.value = value;
-        }
-    }
-
-    class DirectoryAsset : Asset
-    {
-        public string directoryPath { get; }
-
-        public DirectoryAsset(string directoryPath, Version version) : base(AssetType.RegistryValue, version)
-        {
-            this.directoryPath = directoryPath;
-        }
-    }
-
-    class FileAsset : Asset
-    {
-        public string directoryPath { get; }
-        public string fileName { get; }
-
-        public FileAsset(string directoryPath,string fileName, Version version) : base(AssetType.RegistryValue, version)
-        {
-            this.directoryPath = directoryPath;
-            this.fileName = fileName;
-        }
-    }
-
     enum ChangeStatus
     {
         NotStarted,
