7aa7f228b7e4fbe9a14cff1387150b56997dfcc9
Chris Manders <cmanders159@gmail.com>
2017-11-10 17:54:40 +0000

    Added Asset classes

--- AutoUpdate.cs
+++ AutoUpdate.cs
@@ -234,4 +234,83 @@ namespace AutoUpdate
                 manifestFileLocation = versionNode.SelectSingleNode("ManifestFileLocation").Value;
         }
     }
+
+    class Asset
+    {
+        public AssetType type { get; }
+        public Version version { get; }
+
+        public Asset(AssetType type, Version version)
+        {
+            this.type = type;
+            this.version = version;
+        }
+    }
+
+    class RegistrySubKeyAsset : Asset
+    {
+        public string subKeyPath { get; }
+
+        public RegistrySubKeyAsset(string subKeyPath, Version version) : base(AssetType.RegistrySubKey, version)
+        {
+            this.subKeyPath = subKeyPath;
+        }
+    }
+
+    class RegistryValueAsset : Asset
+    {
+        public string subKeyPath { get; }
+        public string value { get; }
+
+        public RegistryValueAsset(string subKeyPath, string value, Version version) : base(AssetType.RegistryValue, version)
+        {
+            this.subKeyPath = subKeyPath;
+            this.value = value;
+        }
+    }
+
+    class DirectoryAsset : Asset
+    {
+        public string directoryPath { get; }
+
+        public DirectoryAsset(string directoryPath, Version version) : base(AssetType.RegistryValue, version)
+        {
+            this.directoryPath = directoryPath;
+        }
+    }
+
+    class FileAsset : Asset
+    {
+        public string directoryPath { get; }
+        public string fileName { get; }
+
+        public FileAsset(string directoryPath,string fileName, Version version) : base(AssetType.RegistryValue, version)
+        {
+            this.directoryPath = directoryPath;
+            this.fileName = fileName;
+        }
+    }
+
+    enum ChangeStatus
+    {
+        NotStarted,
+        InProgress,
+        Successful,
+        Failed,
+        RevertPending,
+        RevertInProgress,
+        RevertSuccessful,
+        RevertFailed
+    }
+
+    /// <summary>
+    /// Details the type of asset that might be listed in a manifest file
+    /// </summary>
+    enum AssetType
+    {
+        RegistrySubKey,
+        RegistryValue,
+        Folder,
+        File
+    }
 }
